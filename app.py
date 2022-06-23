50639de0a8286606d85b552d16b454b53ae886b0
Pranav-NCU <pranav19csu400@gmail.com>
2022-06-23 17:27:04 +0530

    Docker file added

--- app.py
+++ app.py
@@ -10,3 +10,6 @@ def index():
 if __name__=="__main__":
     app.run(debug=True)    
 
+#Heroku_api_name= ml-summer-app
+#heroku_api_key = 377fb4fd-ad47-4cfa-90d9-583a2b25d93a
+#heroku_email = pranav619vashisth@gmail.com
